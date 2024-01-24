248e545661c9f622d6a531439ae2fa80af0549ac
Alexander Sosa Ruiz <87212876+AinsSama20@users.noreply.github.com>
2024-01-24 14:13:01 -0500

    update app.js

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ function asignarTextoElemento(elemento,texto){
 
 function generarNumeroSecreto() {
     let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+    console.log('Numero Secreto', numeroGenerado);
+    listaNumerosSorteadosOrdenada = listaNumerosSorteados.sort((a,b)=>a-b)
+    console.log('Lista de numeros secretos generados',listaNumerosSorteadosOrdenada);
     if (listaNumerosSorteados.length == numeroMaximo) {
         asignarTextoElemento('p','Ya se sortearon todos los numeros posibles');
         document.getElementById('intentar').setAttribute('disabled','true');
@@ -32,6 +35,7 @@ function verificarIntento() {
     if (numeroDeUsuario === numeroSecreto) {
         asignarTextoElemento('p',`Acertaste, numero de intentos: ${intentos} ${(intentos==1) ? 'vez' : 'veces' }, numero secreto: ${numeroSecreto}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById('intentar').setAttribute('disabled','true');
     } else {
         if (numeroDeUsuario > numeroSecreto) {
             asignarTextoElemento('p','El numero secreto es menor');
@@ -56,9 +60,14 @@ function reiniciarJuego() {
 function condicionesIniciales(){
     asignarTextoElemento('h1','Juego del numero secreto');
     asignarTextoElemento('p',`Indica un numero del 1 al ${numeroMaximo}`);
+    document.getElementById('reiniciar').setAttribute('disabled','true');
+    if ((listaNumerosSorteados.length)==numeroMaximo) {
+        document.getElementById('intentar').setAttribute('disabled','true');
+    }else{
+        document.getElementById('intentar').removeAttribute('disabled');
+    }
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
-    document.getElementById('reiniciar').setAttribute('disabled','true');
 }
 
 condicionesIniciales();
